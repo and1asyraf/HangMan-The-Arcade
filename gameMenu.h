7e5a418255a61d20581cfacbe3645d191024fef1
Asyrafdelaghetto <138023630+Asyrafdelaghetto@users.noreply.github.com>
2023-06-28 22:48:27 +0800

    Create gameMenu.h

---
+++ gameMenu.h
@@ -0,0 +1,22 @@
+// gameMenu Header
+#ifndef GAMEMENU_H
+#define GAMEMENU_H
+#include <string>
+
+using namespace std;
+
+class gameMenu
+{
+	private: 
+		string playerName;
+	public: 
+		gameMenu();
+		void getPlayerName();
+		void displayRule();
+		void dispWelcomeMessage();
+		void clearOutput();
+		int displayScore(int& score);
+		string getPlayerName() const;
+};
+
+#endif
