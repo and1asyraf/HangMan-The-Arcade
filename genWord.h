1ea18770b1ee1a68b8c78cfc05ae1ebbc1f8acd2
Asyrafdelaghetto <138023630+Asyrafdelaghetto@users.noreply.github.com>
2023-06-28 22:44:59 +0800

    genWord.h

---
+++ genWord.h
@@ -0,0 +1,16 @@
+//genWord Header 
+#ifndef GENWORD_H
+#define GENWORD_H
+#include <string>
+using namespace std;
+
+class genWord{
+	private:
+    string word;
+  public:
+		genWord(){word = "";}
+		string randomWord();
+		void printWord();
+};
+
+#endif
